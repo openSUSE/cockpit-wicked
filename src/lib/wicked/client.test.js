62344b97982672d0ed35ee7848d3a2d68f99a0b3
Imobach González Sosa <igonzalezsosa@suse.com>
2020-10-28 12:35:00 +0000

    Add a WickedClient to get interfaces and configurations
    * It uses Wicked's CLI to get the data.

---
+++ src/lib/wicked/client.test.js
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) [2020] SUSE LLC
+ *
+ * All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, contact SUSE LLC.
+ *
+ * To contact SUSE LLC about this file by physical or electronic mail, you may
+ * find current contact information at www.suse.com.
+ */
+
+import Client from './client';
+
+const client = new Client();
+
+describe('#getInterfaces', () => {
+    it('returns the active interfaces', () => {
+        expect.assertions(1);
+
+        return client.getInterfaces().then(data => {
+            const names = data.map(i => i.interface.name);
+            expect(names).toEqual(['lo', 'eth0', 'eth1', 'eth2', 'bond0']);
+        });
+    });
+});
+
+describe('#getConfigurations', () => {
+    it('returns configurations', () => {
+        expect.assertions(2);
+
+        return client.getConfigurations().then(data => {
+            const names = data.map(c => c.name);
+            expect(names).toEqual(['lo', 'eth0', 'eth1', 'bond0', 'eth2']);
+
+            // Check list data
+            const bond0 = data.find(c => c.name === 'bond0');
+            const bond0Slaves = bond0.bond.slaves.map(s => s.device);
+            expect(bond0Slaves).toEqual(['eth1', 'eth2'])
+        });
+    });
+})

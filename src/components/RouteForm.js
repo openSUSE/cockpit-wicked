04188afb05bbdcc298e33204763da94846c67982
David Díaz González <dgonzalez@suse.de>
2020-11-11 14:31:07 +0000

    [Routes] Eslint fix

--- src/components/RouteForm.js
+++ src/components/RouteForm.js
@@ -22,7 +22,7 @@
 import React, { useState, useEffect } from 'react';
 import { Button, Checkbox, Form, FormGroup, Modal, ModalVariant, FormSelect, FormSelectOption, TextInput } from '@patternfly/react-core';
 import cockpit from 'cockpit';
-import { useNetworkDispatch, useNetworkState, actionTypes, addRoute, updateRoute } from '../context/network';
+import { useNetworkDispatch, useNetworkState, addRoute, updateRoute } from '../context/network';
 
 const _ = cockpit.gettext;
 
